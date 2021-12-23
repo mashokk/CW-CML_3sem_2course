f365ed1bea3a23c8b9b1cab56838cc4ec01cb75e
Hlushkova Maria <marusia2200@gmail.com>
2021-12-23 22:30:13 +0300

    22:30 23.12.2021

--- Contacts.js
+++ Contacts.js
@@ -7,7 +7,7 @@ window.onload = function () {
     //вешаем на него событие
     a.onclick = function() {
         //производим какие-то действия
-        if (this.innerHTML=='<object data="image/kontakty.svg" type="image/svg+xml"><img src="images/kontakty.svg" alt="Contacts"></object>') this.innerHTML = '+375259914356 <br> marusia2200@gmail.com';
+        if (this.innerHTML=='<object data="image/kontakty.svg" type="image/svg+xml"><img src="images/kontakty.svg" alt="Contacts"></object>') this.innerHTML = '+375251234567 <br> marusia2200@gmail.com';
         else this.innerHTML = '<object data="image/kontakty.svg" type="image/svg+xml"><img src="images/kontakty.svg" alt="Contacts"></object>';
         //предотвращаем переход по ссылке href
         return false;
